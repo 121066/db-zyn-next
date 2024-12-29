5eab053a2bcff9059f67c6c79215288cdc553902
p-dengbin8 <1318720440@qq.com>
2024-12-29 23:59:33 +0800

    fix: 修改配置

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   assetPrefix: '',
-  basePath: '/yn',
+  basePath: '',
 };
 
 export default nextConfig;
