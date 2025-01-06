a9a68a1b94ab17f0c01a93eeee5f024967179f90
p-dengbin8 <1318720440@qq.com>
2025-01-07 00:05:05 +0800

    fix: 修改打包问题

--- src/app/components/MonacoEditor/index.tsx
+++ src/app/components/MonacoEditor/index.tsx
@@ -6,20 +6,20 @@ function MonacoEditors() {
         selectOnLineNumbers: true,
     };
     const [code, setCode] = useState('')
-    const editor = MonacoEditor.create('#editor', {
-        /* 一些配置， 比如说主题，语言类型，等等 */
-        theme: 'vs',
-        /* 语言 */
-        language: 'sql',
-        /* 是否只读 */
-        readOnly: false,
-        /* 初始值 */
-        value: '',
-        /* 自动布局 */
-        automaticLayout: true,
-        /* 阻止编辑器滚动到最后一行之外 */
-        scrollBeyondLastLine: true,
-    });
+    // const editor = MonacoEditor.create('#editor', {
+    //     /* 一些配置， 比如说主题，语言类型，等等 */
+    //     theme: 'vs',
+    //     /* 语言 */
+    //     language: 'sql',
+    //     /* 是否只读 */
+    //     readOnly: false,
+    //     /* 初始值 */
+    //     value: '',
+    //     /* 自动布局 */
+    //     automaticLayout: true,
+    //     /* 阻止编辑器滚动到最后一行之外 */
+    //     scrollBeyondLastLine: true,
+    // });
     return (
         <div>
             <div id="editor"></div>
