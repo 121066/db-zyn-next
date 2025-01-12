3876f5a30d5754541e5457a28f93241526fd6c77
p-dengbin8 <1318720440@qq.com>
2025-01-12 21:51:58 +0800

    fix: 修改打包配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: '/yn/',
+    basePath: '/yn',
     typescript: {
+
         // !! 警告 !!
         // 在生产环境中忽略 TypeScript 错误
         // 这可能很危险，但有时是必要的
